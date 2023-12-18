231f5f386d1b6c3fbebedcf25a971f5354c74f92
vibhor <vibgitcode27@gmail.com>
2023-12-19 03:21:43 +0530

    tested

--- src/app/compnents/comp.tsx
+++ src/app/compnents/comp.tsx
@@ -1,55 +1,70 @@
-"use client"
-import { trpc } from "../_utils/client"
-import { useState } from "react"
-import { z } from "zod"
+"use client";
+import { trpc } from "../_utils/client";
+import { useState } from "react";
+import { z } from "zod";
 
-export function TestFunctionality()
-{
-    let pushUser = trpc.user.signUp.useMutation();
+// Define a schema for user input
+const userInputSchema = z.object({
+  email: z.string(),
+  username: z.string(),
+  password: z.string(),
+  bio: z.string(),
+});
 
-    const [email , setEmail] = useState()
-    const [username , setUsername] = useState()
-    const [password , setPassword] = useState()
-    const [bio , setBio ] = useState()
+export function TestFunctionality() {
+  const pushUser = trpc.user.signUp.useMutation();
 
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    password: "",
+    bio: "",
+  });
 
-    return (
-        <div>
-            <h1>Email</h1>
-            <input type="text" onChange={(event) =>
-            {
-                //@ts-ignore
-                setEmail(event.target.value)
-            }} />
-            <h1>Username</h1>
-            <input type="text" onChange={(event) =>
-            {
-                //@ts-ignore
-                setUsername(event.target.value)
-            }} />
-            <h1>Password</h1>
-            <input type="text" onChange={(event) =>   
-            {
-                //@ts-ignore
-                setPassword(event.target.value)
-            }} />
-            <h1>Bio</h1>
-            <input type="text" onChange={(event) =>
-            {
-                //@ts-ignore
-                setBio(event.target.value)
-            }} />
+  const handleChange = (field: string, value: string) => {
+    setFormData((prevData) => ({ ...prevData, [field]: value }));
+  };
 
-            <button onClick={() =>
-            {
-                pushUser.mutate({
-                    email,
-                    username,
-                    password,
-                    bio
-                })
-                console.log("Works")
-            }}>Submit</button>
-        </div>
-    )
+  const handleSubmit = () => {
+    const { email, username, password, bio } = formData;
+
+    try {
+      userInputSchema.parse({ email, username, password, bio });
+      pushUser.mutate({ email, username, password, bio });
+      console.log("Works");
+    } catch (error) {
+      console.error("Validation error:", error.errors);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Email</h1>
+      <input
+        type="text"
+        value={formData.email}
+        onChange={(event) => handleChange("email", event.target.value)}
+      />
+      <h1>Username</h1>
+      <input
+        type="text"
+        value={formData.username}
+        onChange={(event) => handleChange("username", event.target.value)}
+      />
+      <h1>Password</h1>
+      <input
+        type="text"
+        value={formData.password}
+        onChange={(event) => handleChange("password", event.target.value)}
+      />
+      <h1>Bio</h1>
+      <input
+        type="text"
+        value={formData.bio}
+        onChange={(event) => handleChange("bio", event.target.value)}
+      />
+
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
