f531fcd1052d904e3edf5ffe49bc67e052f11906
Vibhor <vibgitcode27@gmail.com>
2024-01-06 03:02:59 +0530

    finished myProfile page

--- src/app/compnents/PofilePosts.tsx
+++ src/app/compnents/PofilePosts.tsx
@@ -1,9 +1,34 @@
 import React from "react";
 import "../styles/ProfilePage.css"
-export default function PostInMyProfile(){
-    return(
-        <div className="main-content">
-            This is the post page
-        </div>
-    )
-}
+import img1 from "../_assets/image7.jpg"
+import img2 from "../_assets/image4.jpg"
+import img3 from "../_assets/post2.webp"
+import img4 from "../_assets/post1.webp"
+import img5 from "../_assets/image6.png"
+import img6 from "../_assets/image5.jpg"
+import img7 from "../_assets/image3.jpg"
+import img8 from "../_assets/post3.webp"
+
+const imageArray = [
+    img1.src,
+    img2.src,
+    img3.src,
+    img4.src,
+    img5.src,
+    img6.src,
+    img7.src,
+    img8.src,
+    // Add more image URLs as needed
+  ];
+  
+  const PostInMyProfile = () => {
+    return (
+      <div className="post-container">
+        {imageArray.map((imageUrl, index) => (
+          <img key={index} src={imageUrl} alt={`Post Image ${index + 1}`} className="post-image" />
+        ))}
+      </div>
+    );
+  };
+  
+  export default PostInMyProfile;
--- src/app/compnents/ProfileFollowers.tsx
+++ src/app/compnents/ProfileFollowers.tsx
@@ -1,9 +1,58 @@
 import React from "react";
+import { Avatar } from "@mui/material";
+import img4 from "../_assets/image5.jpg"
+import img1 from "../_assets/image7.jpg"
+import img2 from "../_assets/image4.jpg"
+import img3 from "../_assets/image6.png"
+import img5 from "../_assets/image2.jpg"
+import "../styles/ProfileFollowers.css"
 
 export default function FollowersInMyProfile(){
+
+    const fetchedProfile = [
+        {
+            h1 : "Ivanka James",
+            h2 : "@ivankajames",
+            img : img3
+        },
+        {
+            h1 : "Big Bundah Girl",
+            h2 : "@bigBgirl",
+            img : img1 
+        },
+        {
+            h1 : "GlizzyGobbler",
+            h2 : "@GZperiod",
+            img : img2
+        },
+        {
+            h1 : "Tiny Weenie",
+            h2 : "@teeniweeni",
+            img : img4    
+        },
+        {
+            h1 : "Bob Loader",
+            h2 : "@BLnigger",
+            img : img3
+        },
+        {
+            h1 : "Dee Snuts",
+            h2 : "@DjNutter",
+            img : img5
+        }
+    ]
     return(
-        <div>
-            This is the followers page
+        <div className="follower-main-div">
+                {fetchedProfile.map((value, index) => (
+                    <div>
+                    <Avatar alt="Profile Pic" src={value.img.src} style={{position : "relative" , width : "10vh" , height : "10vh" , marginTop : "1vh" , marginLeft : "16vh" , border: "2px solid black"}}/>
+                <div className="follower-profile">
+                    <h1>{value.h1}</h1>
+                    <h2>{value.h2}</h2>
+                </div>
+                <button>Remove</button>
+                </div>))
+                }
         </div>
     )
 }
