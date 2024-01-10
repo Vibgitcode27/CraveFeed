144b8164473c091b00ceb5c0ad3be8f4bcfd054b
Shashwat Singh <shashwat123student@gmail.com>
2024-01-10 07:47:49 +0530

    Fetching Queries perfect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,43 @@
 "use client";
 
-import { signIn, useSession } from 'next-auth/react';
-import { useRouter } from "next/navigation";
-import { trpc } from "@/app/_trpc/client";
-import path from "path";
-import Link from "next/link";
+import { trpc } from './_trpc/client';
+import {z} from "zod";
+import {useQueryClient} from "@tanstack/react-query";
+import {useState} from "react";
 
 export default function Home() {
-    const router = useRouter();
-    const { data: session } = useSession();
+    // const mutation = trpc.newPost.useMutation();
+    // const check = () => {
+    //     mutation.mutate({
+    //         userId: 1 ,
+    //         restaurantName: "KK",
+    //         cityName: "Kanpur",
+    //         dishName: "Matar Panner",
+    //         caption: "Caption This",
+    //         image: "This will be the imamge",
+    //         location: "location",
+    //     })
+    //     // const responseData = mutation.data;
+    //     // console.log("Mutation Data: ",responseData);
+    // }
+    const [inputValue, setInputValue] = useState('');
+    const { data: users, isLoading, isFetching } = trpc.authSignInCheck.useQuery({ username: "shashwat123student@gmail.com" });
 
-    async function handleSignIn() {
-        await signIn();
-        // if (session) {
-        //     const mutation = trpc.signInCheck.useQuery({
-        //         username: session.user.email!,
-        //     });
-        //     const response = mutation.data;
-        //     if (response && response.message) {
-        //         router.push("/post");
-        //     } else {
-        //         router.push("/test");
-        //     }
-        // } else {
-        //     router.push("/signIn");
-        // }
+    const queryClient = useQueryClient();
+
+    function handleClick() {
+        console.log(users);
     }
 
-    return (
+    if (isLoading || isFetching) {
+        return <p>Loading...</p>;
+    }
+    return(
         <div>
-            <button
-                onClick={handleSignIn}
-                className="flex items-center justify-center gap-2 rounded-lg bg-teal-500 px-8 py-3 text-center text-sm font-semibold text-white ring-teal-300 transition duration-100 hover:bg-teal-600 md:text-base"
-            >
-                Login
+            <h1>Button for checking</h1>
+            <button onClick={handleClick}>
+                Check for Mutation
             </button>
-
-            {session && (
-                <div>
-                    <p>Welcome, {session.user.name}!</p>
-                    <p>{session.user.email}</p>
-                    <img src={session.user.image} alt={"Github Image"}></img>
-                </div>
-            )}
         </div>
-    );
+    )
 }
